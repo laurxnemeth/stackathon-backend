e0cb31fbdf302995c448d71f0956c77ffb48eb6b
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-01-24 11:02:19 -0500

    feat: router handling

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const Koa = require('koa');
 const server = new Koa();
+const logger = require('koa-morgan');
+const Router = require('koa-router');
+
+const router = new Router();
+
+router.get('/', ctx => {
+    ctx.body = 'I am  root!'
+});
+
+router.get('/second_route', ctx => {
+    ctx.body = 'I am your recond route'
+});
+
+router.post('/something', ctx => {
+    ctx.body = {
+        something: 'something here'
+    }
+})
 
 server
-    .use(ctx => {
-    ctx.body = 'I am your first Koa API!'
-})
-    .listen(2020)
+    .use(logger('tiny'))
+    .use(router.routes())
+    .listen(2020);
