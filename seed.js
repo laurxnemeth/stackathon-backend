5077ffa3744917e535b11fa841f306b55eddcc3f
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-01-25 22:42:43 -0500

    feat: seed works!

--- seed.js
+++ seed.js
@@ -1,7 +1,19 @@
 const db = require('./db')
+const { getBook, excerptMaker } = require('./server/helpers.js');
+const { Book, Excerpt } = require('./db/models/')
 
 const seed = async () => {
   await db.sync({force: true})
+
+  const books = await Promise.all([
+		Book.create({ title: 'Little Women', author: 'Louisa May Alcott' })
+	]);
+
+const addExcerpt = [];
+excerptMaker(getBook('public/littleWomen.txt')).forEach((excerpt) => {
+  addExcerpt.push(Excerpt.create({paragraph: excerpt, bookId: 1}));
+})
+  const excerpts = await Promise.all(addExcerpt);
 }
 
 seed().catch((err) => {
